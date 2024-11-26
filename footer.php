585fbd59ad2318a19be6397438e370df9cb37864
khairulaimnn <160590845+khairulaimnn@users.noreply.github.com>
2024-11-26 11:22:22 +0800

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,38 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <h3>Quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> Home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> About</a>
+         <a href="shop.php"> <i class="fas fa-angle-right"></i> Shop</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i> Contact</a>
+      </div>
+
+      <div class="box">
+         <h3>Extra links</h3>
+         <a href="user_login.php"> <i class="fas fa-angle-right"></i> Login</a>
+         <a href="user_register.php"> <i class="fas fa-angle-right"></i> Register</a>
+         <a href="cart.php"> <i class="fas fa-angle-right"></i> Cart</a>
+         <a href="orders.php"> <i class="fas fa-angle-right"></i> Orders</a>
+      </div>
+
+      <div class="box">
+         <h3>Contact Us.</h3>
+         <a href="tel:9800000000"><i class="fas fa-phone"></i> 03-8000 8000</a>
+         <a href="mailto:khairul.5002@gmail.com"><i class="fas fa-envelope"></i> uniqtech@gmail.com</a>
+         <a href="https://www.google.com/myplace"><i class="fas fa-map-marker-alt"></i> Kuala Lumpur, Malaysia </a>
+      </div>
+
+      <div class="box">
+         <h3>Follow Us</h3>
+         <a href="https://x.com/khaiaimn" target="_blank"><i class="fab fa-twitter"></i>Twitter</a>
+         <a href="https://www.instagram.com/harshchy__/" target="_blank"><i class="fab fa-instagram"></i>Instagram</a>
+         </i></div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?><span></span> | all rights reserved</div>
+
+</footer>
